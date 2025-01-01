ebfc0eb7a1ece504e66c2e04fd30f74625639b67
Vidith Agarwal <vidithagarwal315@gmail.com>
2025-01-01 18:12:29 -0500

    Added Validation to detect mathematical characters

--- server/src/controllers/romanConvertorController.js
+++ server/src/controllers/romanConvertorController.js
@@ -1,11 +1,9 @@
 import { intToRoman } from '../services/romanConvertorService.js';
-import { createError } from '../middleware/errorMiddleware.js';
+import { validateInput } from '../utils/validator.js';
 export const getRomanNumeral = (req, res, next) => {
   try {
     const query = req.query.query;
-    if (!query || isNaN(query)) {
-      throw createError(400, 'Invalid input: Please provide a numeric value.');
-    }
+    validateInput(query);
     const number = parseInt(query);
     const output = intToRoman(number);
     res.json({ input: query.toString(), output: output.toString() });
--- server/src/utils/validator.js
+++ server/src/utils/validator.js
@@ -1,4 +1,12 @@
+import { createError } from '../middleware/errorMiddleware.js';
+
 // Utility for validating input (e.g., range checks)
 export const isInRange = (num) => {
     return num >= 1 && num <= 3999;
+};
+
+export const validateInput = (input) => {
+    if (!input || isNaN(input) || (/[^0-9-]/.test(input))) {
+        throw createError(400, 'Invalid input: Please provide a numeric value.');
+    }
 };
