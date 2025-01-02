8a854f5d62d5346b9d84ffceccb4e4f6c3d3b22c
Vidith Agarwal <vidithagarwal315@gmail.com>
2025-01-01 22:16:25 -0500

    Changed the backend url for production

--- client/src/services/apiService.js
+++ client/src/services/apiService.js
@@ -1,5 +1,6 @@
 const fetchRomanNumeral = async (query) => {
-    const response = await fetch(`http://localhost:8080/romannumeral?query=${query}`);
+    const baseUrl = import.meta.env.VITE_API_URL || 'http://localhost:8080';
+    const response = await fetch(`${baseUrl}/romannumeral?query=${query}`);
     if (!response.ok) {
       const errorData = await response.json();
       throw new Error(errorData.error);
